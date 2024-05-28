9410a255be3fd4f6beee8db8d67ff0c6e8222fe6
muhammadnaeemch2 <muhammadnaeemch2@gmail.com>
2024-05-28 13:27:37 +0500

    Active link in nextJs
    Add the basic css for the active link.
    Check whehter the link is active or not using usePathname().

--- src/app/(auth)/(with-auth-layout)/layout.tsx
+++ src/app/(auth)/(with-auth-layout)/layout.tsx
@@ -1,14 +1,33 @@
-import React from 'react';
+'use client'
+import React from "react";
+import './style.css'
+import Link from "next/link";
+import { usePathname } from "next/navigation";
 
-const AuthLayout = ({children}:Readonly<{
-    children: React.ReactNode;
-  }>) => {
-    return(
-        <>
-        <h3>Auth Pages Layout</h3>
-        {children}
-        </>
-    )
+const navLinks = [
+  { name: "login", href: "/login" },
+  { name: "register", href: "/register" },
+];
+
+const AuthLayout = ({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) => {
+  const pathName = usePathname()
+  return (
+    <div>
+      {navLinks.map((link) => {
+        const isActive = pathName.startsWith(link.href)
+        return(
+          <Link href={link.href} key={link.name} className={isActive ? "font-bold mr-4":"text-blue-500 mr-4"}>
+          {link.name}
+        </Link>
+        )
+      })}
+      {children}
+    </div>
+  );
 };
 
 export default AuthLayout;
